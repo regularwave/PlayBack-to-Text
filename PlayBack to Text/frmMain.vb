2aeccafd23f4866644549c2f1bf252afb7e04f38
regularwave <regularwave@gmail.com>
2017-08-03 23:56:26 -0400

    Rewrite of first go at this program.

--- PlayBack to Text/frmMain.vb
+++ PlayBack to Text/frmMain.vb
@@ -1,3 +1,27 @@
 ﻿Public Class fromMain
+    Private Sub fromMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If 
+    End Sub
+End Class
+
+Public Class AppFunctions
+    'a class for different functions
+    'use by calling Globals.FuncHold.[function]
+
+    Function CheckForOutputPath() As Boolean
+        If My.Settings.OutputhPath = "" Or vbNullString Then
+            Return False
+        Else
+            Return True
+        End If
+
+    End Function
+
+
+End Class
+
+Public Class Globals
+    Public Shared FuncHold As New AppFunctions
+
 
 End Class
