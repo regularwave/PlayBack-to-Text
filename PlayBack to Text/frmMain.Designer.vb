d260fabe5db6d8e9280f9c6128c47f82e6bf8aa5
regularwave <regularwave@gmail.com>
2017-08-07 00:31:41 -0400

    rewritten!

--- PlayBack to Text/frmMain.Designer.vb
+++ PlayBack to Text/frmMain.Designer.vb
@@ -22,45 +22,52 @@ Partial Class fromMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.txtOutputDir = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
         Me.btnChangeOutputPath = New System.Windows.Forms.Button()
         Me.txtJSONDir = New System.Windows.Forms.TextBox()
         Me.lblOutput = New System.Windows.Forms.Label()
         Me.btnFindJSON = New System.Windows.Forms.Button()
         Me.lblJSON = New System.Windows.Forms.Label()
+        Me.tInterval = New System.Windows.Forms.Timer(Me.components)
+        Me.nudInterval = New System.Windows.Forms.NumericUpDown()
+        Me.lblInterval = New System.Windows.Forms.Label()
+        Me.btnStop = New System.Windows.Forms.Button()
+        CType(Me.nudInterval, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txtOutputDir
         '
-        Me.txtOutputDir.Location = New System.Drawing.Point(51, 12)
+        Me.txtOutputDir.Location = New System.Drawing.Point(46, 32)
         Me.txtOutputDir.Name = "txtOutputDir"
         Me.txtOutputDir.ReadOnly = True
-        Me.txtOutputDir.Size = New System.Drawing.Size(147, 20)
+        Me.txtOutputDir.Size = New System.Drawing.Size(222, 20)
         Me.txtOutputDir.TabIndex = 0
         '
-        'Button1
+        'btnReset
         '
-        Me.Button1.Location = New System.Drawing.Point(21, 413)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(135, 85)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "reset"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnReset.Location = New System.Drawing.Point(277, 58)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(20, 23)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "X"
+        Me.btnReset.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnStart
         '
-        Me.Button2.Location = New System.Drawing.Point(21, 322)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(135, 85)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "check"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnStart.Enabled = False
+        Me.btnStart.Location = New System.Drawing.Point(103, 58)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(79, 23)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
         '
         'btnChangeOutputPath
         '
-        Me.btnChangeOutputPath.Location = New System.Drawing.Point(204, 12)
+        Me.btnChangeOutputPath.Location = New System.Drawing.Point(274, 33)
         Me.btnChangeOutputPath.Name = "btnChangeOutputPath"
         Me.btnChangeOutputPath.Size = New System.Drawing.Size(26, 20)
         Me.btnChangeOutputPath.TabIndex = 1
@@ -69,16 +76,16 @@ Partial Class fromMain
         '
         'txtJSONDir
         '
-        Me.txtJSONDir.Location = New System.Drawing.Point(51, 38)
+        Me.txtJSONDir.Location = New System.Drawing.Point(46, 6)
         Me.txtJSONDir.Name = "txtJSONDir"
         Me.txtJSONDir.ReadOnly = True
-        Me.txtJSONDir.Size = New System.Drawing.Size(147, 20)
+        Me.txtJSONDir.Size = New System.Drawing.Size(222, 20)
         Me.txtJSONDir.TabIndex = 2
         '
         'lblOutput
         '
         Me.lblOutput.AutoSize = True
-        Me.lblOutput.Location = New System.Drawing.Point(9, 15)
+        Me.lblOutput.Location = New System.Drawing.Point(5, 35)
         Me.lblOutput.Name = "lblOutput"
         Me.lblOutput.Size = New System.Drawing.Size(39, 13)
         Me.lblOutput.TabIndex = 3
@@ -86,7 +93,7 @@ Partial Class fromMain
         '
         'btnFindJSON
         '
-        Me.btnFindJSON.Location = New System.Drawing.Point(204, 38)
+        Me.btnFindJSON.Location = New System.Drawing.Point(274, 7)
         Me.btnFindJSON.Name = "btnFindJSON"
         Me.btnFindJSON.Size = New System.Drawing.Size(26, 20)
         Me.btnFindJSON.TabIndex = 1
@@ -96,38 +103,79 @@ Partial Class fromMain
         'lblJSON
         '
         Me.lblJSON.AutoSize = True
-        Me.lblJSON.Location = New System.Drawing.Point(10, 42)
+        Me.lblJSON.Location = New System.Drawing.Point(5, 9)
         Me.lblJSON.Name = "lblJSON"
         Me.lblJSON.Size = New System.Drawing.Size(35, 13)
         Me.lblJSON.TabIndex = 3
         Me.lblJSON.Text = "JSON"
         '
+        'tInterval
+        '
+        Me.tInterval.Interval = 5000
+        '
+        'nudInterval
+        '
+        Me.nudInterval.Location = New System.Drawing.Point(48, 59)
+        Me.nudInterval.Maximum = New Decimal(New Integer() {180, 0, 0, 0})
+        Me.nudInterval.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nudInterval.Name = "nudInterval"
+        Me.nudInterval.Size = New System.Drawing.Size(49, 20)
+        Me.nudInterval.TabIndex = 4
+        Me.nudInterval.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'lblInterval
+        '
+        Me.lblInterval.AutoSize = True
+        Me.lblInterval.Location = New System.Drawing.Point(5, 61)
+        Me.lblInterval.Name = "lblInterval"
+        Me.lblInterval.Size = New System.Drawing.Size(42, 13)
+        Me.lblInterval.TabIndex = 3
+        Me.lblInterval.Text = "Interval"
+        '
+        'btnStop
+        '
+        Me.btnStop.Enabled = False
+        Me.btnStop.Location = New System.Drawing.Point(189, 58)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(79, 23)
+        Me.btnStop.TabIndex = 1
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
         'fromMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(471, 559)
+        Me.ClientSize = New System.Drawing.Size(309, 89)
+        Me.Controls.Add(Me.nudInterval)
         Me.Controls.Add(Me.lblJSON)
+        Me.Controls.Add(Me.lblInterval)
         Me.Controls.Add(Me.lblOutput)
         Me.Controls.Add(Me.txtJSONDir)
         Me.Controls.Add(Me.btnFindJSON)
         Me.Controls.Add(Me.btnChangeOutputPath)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnReset)
         Me.Controls.Add(Me.txtOutputDir)
         Me.Name = "fromMain"
         Me.Text = "PlayBack to Text"
+        CType(Me.nudInterval, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents txtOutputDir As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnStart As Button
     Friend WithEvents btnChangeOutputPath As Button
     Friend WithEvents txtJSONDir As TextBox
     Friend WithEvents lblOutput As Label
     Friend WithEvents btnFindJSON As Button
     Friend WithEvents lblJSON As Label
+    Friend WithEvents tInterval As Timer
+    Friend WithEvents nudInterval As NumericUpDown
+    Friend WithEvents lblInterval As Label
+    Friend WithEvents btnStop As Button
 End Class
--- PlayBack to Text/frmMain.vb
+++ PlayBack to Text/frmMain.vb
@@ -1,40 +1,96 @@
 ﻿Public Class fromMain
     Private Sub fromMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+
         'check to see if this is the first time the program has been run
         GUse.aFunc.CheckForFirstRun()
-        GUse.aFunc.SetPlaybackJSONPath()
 
-        'if this is the first time the program is run, my.settings.outputpath is blank
-        'so set it to program directory; after first run, don't change
+        'check to see if playback.json exists and set its path
+        If GUse.aFunc.DoesPlaybackJSONExist() Then
+            GUse.aFunc.SetPlaybackJSONPath()
+            txtJSONDir.Text = My.Settings.JSONPath
+            lblJSON.ForeColor = Color.Green
+            GVar.jrdy = True
+        Else
+            txtJSONDir.Text = "Find playback.json!"
+            lblJSON.ForeColor = Color.Red
+        End If
+
+        'check to see if the output path is valid and set its path
         If GUse.aFunc.CheckForOutputPath() Then
             txtOutputDir.Text = My.Settings.OutputPath
             lblOutput.ForeColor = Color.Green
+            GVar.ordy = True
         Else
-            txtOutputDir.Text = "output path isn't set"
+            txtOutputDir.Text = "Choose valid output path!"
             lblOutput.ForeColor = Color.Red
         End If
+
+        'if playback.json has been found and a valid path is set, enable start button
+        btnStart.Enabled = GUse.aFunc.AllReady()
+
     End Sub
 
-    Private Sub btnChangeOutputPath_Click(sender As Object, e As EventArgs) Handles btnChangeOutputPath.Click, btnFindJSON.Click
+    Private Sub btnChangeOutputPath_Click(sender As Object, e As EventArgs) Handles btnChangeOutputPath.Click
         'get new dir and print
         My.Settings.OutputPath = GUse.aFunc.ChooseOutputFolder
         txtOutputDir.Text = My.Settings.OutputPath
+
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
         'clears stored paths and resets timer interval to 5 secs
-        My.Settings.Reset()
+        If MsgBox("This will clear all stored paths and reset the timer interval to 5 seconds." & vbNewLine & "Are you sure?", MsgBoxStyle.YesNo, "Reset?") = MsgBoxResult.Yes Then
+            My.Settings.Reset()
+            MsgBox("Settings reset!", vbCritical)
+        End If
+
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        'check outputpath
-        If GUse.aFunc.CheckForOutputPath() Then
-            txtOutputDir.Text = My.Settings.OutputPath
-            lblOutput.ForeColor = Color.Green
-        Else
-            txtOutputDir.Text = "output path isn't set"
-            lblOutput.ForeColor = Color.Red
-        End If
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        'calls the converter once and then enables the timer & stop, disables start & file funcs
+        GUse.aFunc.Converter()
+        btnStart.Enabled = False
+        btnFindJSON.Enabled = False
+        btnChangeOutputPath.Enabled = False
+        btnReset.Enabled = False
+        btnStop.Enabled = True
+        tInterval.Enabled = True
+
+    End Sub
+
+    Private Sub btnFindJSON_Click(sender As Object, e As EventArgs) Handles btnFindJSON.Click
+        'shouldn't be needed, but allows custom playback.json dir
+        My.Settings.JSONPath = GUse.aFunc.FindPlaybackFile
+
+    End Sub
+
+    Private Sub nudInterval_ValueChanged(sender As Object, e As EventArgs) Handles nudInterval.ValueChanged
+        'changes the interval at which playback.json is checked
+        My.Settings.TimerInterval = nudInterval.Value * 1000
+        tInterval.Interval = My.Settings.TimerInterval
+
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        'disables the timer & stop, enables the start & file funcs, writes " - " to txt file
+        tInterval.Enabled = False
+        btnStop.Enabled = False
+        btnStart.Enabled = True
+        btnFindJSON.Enabled = True
+        btnChangeOutputPath.Enabled = True
+        btnReset.Enabled = True
+
+        Using sw As New IO.StreamWriter(My.Settings.OutputPath & "\playback.txt")
+            sw.WriteLine(" - ")
+        End Using
+
+    End Sub
+
+    Private Sub tInterval_Tick(sender As Object, e As EventArgs) Handles tInterval.Tick
+        'calls the converter each tick
+        'interval is settings.timerinterval
+        GUse.aFunc.Converter()
+
     End Sub
 End Class
 
@@ -49,6 +105,7 @@ Public Class AppFunctions
             My.Settings.OutputPath = GVar.homedirpath
             My.Settings.firstrun = False
         End If
+
     End Sub
 
     Function CheckForOutputPath() As Boolean
@@ -61,10 +118,22 @@ Public Class AppFunctions
 
     End Function
 
+    Function DoesPlaybackJSONExist() As Boolean
+        'checks if playback.json exists in the default location
+        If My.Computer.FileSystem.FileExists(GVar.defjsonloc) Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
     Sub SetPlaybackJSONPath()
         'sets settings.jsonpath to GPMDP's default json store
-        Dim appdata As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
-        My.Settings.JSONPath = appdata & "\Google Play Music Desktop Player\json_store"
+        If My.Computer.FileSystem.FileExists(GVar.defjsonloc) Then
+            My.Settings.JSONPath = GVar.defjsonloc
+        End If
+
     End Sub
 
     Function ChooseOutputFolder() As String
@@ -77,7 +146,71 @@ Public Class AppFunctions
         Else
             Return GVar.homedirpath
         End If
+
     End Function
+
+    Function FindPlaybackFile() As String
+        'should never be needed; GPMDP only stores playback.json in one place
+        Dim filedlog As New OpenFileDialog()
+
+        filedlog.InitialDirectory = GVar.defjsondir
+        filedlog.Filter = "playback.json|playback.json"
+        filedlog.RestoreDirectory = True
+        filedlog.Multiselect = False
+
+        If filedlog.ShowDialog() = DialogResult.OK Then
+            If Dir(filedlog.FileName) <> "" Then
+                Return filedlog.FileName
+            Else
+                Return GVar.defjsonloc
+            End If
+        Else
+            Return GVar.defjsonloc
+        End If
+
+    End Function
+
+    Function AllReady() As Boolean
+        'has the json been found AND there's a valid output path?
+        If GVar.jrdy And GVar.ordy Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Function Stringalizer() As String
+        'converts playback.json to a "song - artist" string and returns it
+        Dim stringJSON As String = GUse.aFunc.JSONtoString()
+        Dim parsedNP = JsonConvert.DeserializeObject(Of NowPlaying)(stringJSON)
+        Dim strtowrite As String = parsedNP.song.title & " - " & parsedNP.song.artist
+
+        Return strtowrite
+
+    End Function
+
+    Function JSONtoString() As String
+        'class-ify the json
+        Dim filereader As String = ""
+
+        Try
+            filereader = My.Computer.FileSystem.ReadAllText(My.Settings.JSONPath)
+        Catch ex As Exception
+        End Try
+
+        Return filereader
+
+    End Function
+
+    Sub Converter()
+        'writes output text to playback.txt
+        Using sw As New IO.StreamWriter(My.Settings.OutputPath & "\playback.txt")
+            sw.WriteLine(GUse.aFunc.Stringalizer())
+        End Using
+
+    End Sub
+
 End Class
 
 Public Class GUse
@@ -90,5 +223,37 @@ End Class
 Public Class GVar
     'global variables
     Public Shared homedirpath As String = My.Application.Info.DirectoryPath
+    Public Shared defjsonloc As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Google Play Music Desktop Player\json_store\playback.json"
+    Public Shared defjsondir As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Google Play Music Desktop Player\json_store"
+    Public Shared jrdy As Boolean = False
+    Public Shared ordy As Boolean = False
 
+End Class
+
+Public Class Song
+    Public Property title As String
+    Public Property artist As String
+    Public Property album As String
+    Public Property albumArt As String
+End Class
+
+Public Class Rating
+    Public Property liked As Boolean
+    Public Property disliked As Boolean
+End Class
+
+Public Class Time
+    Public Property current As Integer
+    Public Property total As Integer
+End Class
+
+Public Class NowPlaying
+    Public Property playing As Boolean
+    Public Property song As Song
+    Public Property rating As Rating
+    Public Property time As Time
+    Public Property songLyrics As String
+    Public Property shuffle As String
+    Public Property repeat As String
+    Public Property volume As Integer
 End Class
